6eec879b74d320a7774c40c8cffbdc125b1afd75
rcharp24 <robj.charpentier@gmail.com>
2025-01-31 12:08:44 -0600

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
